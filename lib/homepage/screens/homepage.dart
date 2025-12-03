7d6c996c9f8fc731a75bd9af28b3bbabe7534c13
Michael Stephen Daniel Panjaitan <michael.stephen@ui.ac.id>
2025-12-03 14:34:47 +0700

    Blank Homepage w/ appbar & drawer

--- lib/homepage/screens/homepage.dart
+++ lib/homepage/screens/homepage.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:Prime-Basket-Place-Mobile/custom/custom_app_bar.dart';
+import 'package:prime_basket_place_mobile/custom/custom_app_bar.dart';
+import 'package:prime_basket_place_mobile/custom/custom_drawer.dart';
 
 class Homepage extends StatelessWidget {
-	Homepage({super.key});
+	const Homepage({super.key});
 
 	@override
 	Widget build(BuildContext context) {
 		return Scaffold(
-			appBar: CustomShopAppBar();
-			body: 
+			appBar: CustomShopAppBar(),
+			drawer: LeftDrawer(),
+			body: Padding(
+				padding: const EdgeInsets.all(8.0),
+
+			),
+
 		);
 	}
 }
