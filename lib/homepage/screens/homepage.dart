6a6a6e500deefaef9795d85ab453e4f61666d56c
Michael Stephen Daniel Panjaitan <michael.stephen@ui.ac.id>
2025-12-03 14:08:42 +0700

    Setup homepage branch

---
+++ lib/homepage/screens/homepage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:Prime-Basket-Place-Mobile/custom/custom_app_bar.dart';
+
+class Homepage extends StatelessWidget {
+	Homepage({super.key});
+
+	@override
+	Widget build(BuildContext context) {
+		return Scaffold(
+			appBar: CustomShopAppBar();
+			body: 
+		);
+	}
+}
